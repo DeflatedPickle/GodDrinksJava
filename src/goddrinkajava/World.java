dfbf9816006cfa46abefc804c1998d33d6a0a119
DeflatedPickle <DeflatedPickle@gmail.com>
2018-03-17 00:44:46 +0000

    Added World class

---
+++ src/goddrinkajava/World.java
@@ -0,0 +1,15 @@
+package goddrinkajava;
+
+public class World {
+    public World(int i) {
+
+    }
+
+    public void addThing(Thing thing) {
+
+    }
+
+    public void startSimulation() {
+
+    }
+}
